e7407f81bb6b9d974d926ac894c3a2cd91894532
elyonjohn <elyonjohn60@gmail.com>
2024-03-09 15:45:28 +0100

    Added Comment

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ let isError = false;
 
 function cleanInputString (str){
     const strArray = str.split('');
-    const cleanstrArray = [];
+    const cleanStrArray = [];
 
     for (let i=0; i<strArray.length; i++){
-        
+        //The .includes() method returns true if the array contains the character, and false if not. 
+        //The logical NOT operator (!) will return the opposite of the value of the .includes() method
+        if(!["+", "-", " "].includes(strArray[i]))
+        cleanStrArray.push(strArray[i]);
     }
 }
