26db8498bad3fafd927c3651e6120a4dfee67762
elyonjohn <elyonjohn60@gmail.com>
2024-03-10 21:59:25 +0100

    Removed existing code within the cleanInputString function. Declared a regex variable and assign it to a value

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString (str){
-    const strArray = str.split('');
-    const cleanStrArray = [];
+    // const strArray = str.split('');
+    // const cleanStrArray = [];
 
-    for (let i=0; i<strArray.length; i++){
-        //The .includes() method returns true if the array contains the character, and false if not. 
-        //The logical NOT operator (!) will return the opposite of the value of the .includes() method
-        if(!["+", "-", " "].includes(strArray[i]))
-        cleanStrArray.push(strArray[i]);
-    }
+    // for (let i=0; i<strArray.length; i++){
+    //     //The .includes() method returns true if the array contains the character, and false if not. 
+    //     //The logical NOT operator (!) will return the opposite of the value of the .includes() method
+    //     if(!["+", "-", " "].includes(strArray[i]))
+    //     cleanStrArray.push(strArray[i]);
+    // }
+
+    const regex = /hello/;  //While looping through the string works, creating a new array is inefficient for memory and runtime performance.
+    //Regular Expressions (referred to as "regex") to match specific characters.
+
+    
 }
