4c25330968c42268e2d2f159e61804ba312610f1
elyonjohn <elyonjohn60@gmail.com>
2024-03-23 18:23:21 +0100

    Comment Added Comments for description

--- script.js
+++ script.js
@@ -208,6 +208,9 @@ function clearForm() {
 
     budgetNumberInput.value = '';
     output.innerText = '';
+    //The difference between innerText and innerHTML
+    //is that innerText will not render HTML elements,
+    //but will display the tags and content as raw text.
 }
 
 addEntryButton.addEventListener("click",addEntry);
