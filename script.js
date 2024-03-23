da47dbbab55ff4ae0e73913a136953851cc8219d
elyonjohn <elyonjohn60@gmail.com>
2024-03-23 17:41:37 +0100

    inputContainers query selector Wrapped the inputContainers query selector in Array.from().

--- script.js
+++ script.js
@@ -196,7 +196,7 @@ function getCaloriesFromInputs(list){
 }
 
 function clearForm() {
-    const inputContainers = document.querySelectorAll('.input-container'); 
+    const inputContainers = Array.from(document.querySelectorAll('.input-container')); 
 }
 
 addEntryButton.addEventListener("click",addEntry);
